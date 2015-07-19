b26a459f3bbb26b9919a920bd8ca0d4ba12c5b18
Dan Tilden <dtilden@sicom.com>
2015-07-19 11:45:01 -0400

    Fixes to settings

--- src/settings.c
+++ src/settings.c
@@ -1,16 +1,15 @@
 #include "settings.h"
 
-
 void Settings_init() {
   // first, check if we have any saved settings
   if (persist_exists(SETTINGS_PERSIST_KEY)) {
     // if we do, what version of the settings was saved?
     uint32_t savedVersion = persist_read_int(SETTINGS_VERSION_KEY);
-    
+
     if(savedVersion == 2) { // in this case, we can direclty load the current version
       persist_read_data(SETTINGS_PERSIST_KEY, &globalSettings, sizeof(Settings));
     } else { // in this case, we're upgrading from the old version
-      
+
         // first, read in the settings using the original format
         Settings_v0 oldSettings;
         persist_read_data(SETTINGS_PERSIST_KEY, &oldSettings, sizeof(Settings_v0));
@@ -26,14 +25,14 @@ void Settings_init() {
         Settings_loadV2Defaults();
     }
   } else {
-    // if there weren't any saved settings, load the defaults 
+    // if there weren't any saved settings, load the defaults
     Settings_loadAllDefaults();
   }
 }
 
 void Settings_loadAllDefaults() {
   // if no persistent data is available, use the defaults
-  
+
   // v1 settings
   #ifdef PBL_COLOR
     globalSettings.timeColor      = GColorOrange;
@@ -45,17 +44,17 @@ void Settings_loadAllDefaults() {
   globalSettings.timeBgColor    = GColorBlack;
   globalSettings.sidebarOnRight = true;
   globalSettings.useMetric      = false;
-  
+
   Settings_loadV2Defaults();
 }
 
 void Settings_loadV2Defaults() {
   // load the v2-added defaults
-  
+
   // settings added in v2
   globalSettings.sidebarTextColor   = GColorBlack;
   globalSettings.btVibe             = false;
-  globalSettings.languageId         = LANGUAGE_ID_ENGLISH;
+  globalSettings.languageId         = LANGUAGE_EN;
   globalSettings.showBatteryLevel   = false;
 }
 
--- src/settings.h
+++ src/settings.h
@@ -1,15 +1,19 @@
 #pragma once
 #include <pebble.h>
-  
+
 #define SETTINGS_PERSIST_KEY 3
 #define SETTINGS_VERSION_KEY 4
-  
+
 // settings for app version 1.2
 #define CURRENT_SETTINGS_VERSION 2
-  
-// available languages (not yet implemented)
-#define LANGUAGE_ID_ENGLISH 0
-  
+
+// language ids
+#define LANGUAGE_EN 0
+#define LANGUAGE_FR 1
+#define LANGUAGE_DE 2
+#define LANGUAGE_ES 3
+#define LANGUAGE_IT 4
+
 typedef struct {
   GColor timeColor;
   GColor timeBgColor;
@@ -22,12 +26,12 @@ typedef struct {
   GColor timeColor;
   GColor timeBgColor;
   GColor sidebarColor;
-  GColor sidebarTextColor; //not yet used
+  GColor sidebarTextColor;
   bool useMetric;
   bool sidebarOnRight;
   bool btVibe;
   bool showBatteryLevel; // not yet used
-  uint8_t languageId; //not yet used
+  uint8_t languageId;
 } Settings;
 
 Settings globalSettings;
