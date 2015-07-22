30c1850b4cc815461c4e89e9766b0ef7754d80a6
Dan Tilden <dtilden@sicom.com>
2015-07-22 10:18:42 -0400

    Added new languages

--- src/languages.h
+++ src/languages.h
@@ -5,22 +5,33 @@
 #define LANGUAGE_DE 2
 #define LANGUAGE_ES 3
 #define LANGUAGE_IT 4
+#define LANGUAGE_NL 5
+#define LANGUAGE_TR 6
+#define LANGUAGE_CZ 7
+#define LANGUAGE_PT 8
 
-
-/* day names in 5 different languages! */
-char dayNames[5][7][8] = {
+/* day names in many different languages! */
+char dayNames[9][7][8] = {
 	{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"},
 	{"DIM", "LUN", "MAR", "MER", "JEU", "VEN", "SAM"},
 	{"SO", "MO", "DI", "MI", "DO", "FR", "SA"},
 	{"DOM", "LUN", "MAR", "MIÉ", "JUE", "VIE", "SÁB"},
-	{"DOM", "LUN", "MAR", "MER", "GIO", "VEN", "SAB"}
+	{"DOM", "LUN", "MAR", "MER", "GIO", "VEN", "SAB"},
+	{"ZO", "MA", "DI", "WO", "DO", "VR", "ZA"},
+	{"PAZ", "PTS", "SAL", "ÇAR", "PER", "CUM", "CTS"},
+	{"NE", "PO", "ÚT", "ST", "ČT", "PÁ", "SO"},
+	{"DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"}
 };
 
-/* month names in 5 different languages! */
-char monthNames[5][12][8] = {
+/* month names in many different languages! */
+char monthNames[9][12][8] = {
 	{"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"},
 	{"JAN", "FÉV", "MAR", "AVR", "MAI", "JUI", "JUL", "AOÛ", "SEP", "OCT", "NOV", "DÉC"},
 	{"JAN", "FEB", "MÄR", "APR", "MAI", "JUN", "JUL", "AUG", "SEP", "OKT", "NOV", "DEZ"},
 	{"ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEP", "OCT", "NOV", "DIC"},
-	{"GEN", "FEB", "MAR", "APR", "MAG", "GIU", "LUG", "AGO", "SET", "OTT", "NOV", "DIC"}
+	{"GEN", "FEB", "MAR", "APR", "MAG", "GIU", "LUG", "AGO", "SET", "OTT", "NOV", "DIC"},
+	{"JAN", "FEB", "MRT", "APR", "MEI", "JUN", "JUL", "AUG", "SEP", "OKT", "NOV", "DEC"},
+	{"OCA", "ŞUB", "MAR", "NİS", "MAY", "HAZ", "TEM", "AĞU", "EYL", "EKİ", "KAS", "ARA"},
+	{"LED", "ÚNO", "BŘE", "DUB", "KVĚ", "ČRV", "ČVC", "SRP", "ZÁŘ", "ŘÍJ", "LIS", "PRO"},
+	{"JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"}
 };
