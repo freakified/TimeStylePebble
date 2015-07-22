e5f70dd2bcc6ba747d330817389e1f172a77ed50
Dan Tilden <dtilden@sicom.com>
2015-07-22 15:33:12 -0400

    Experimental greek language support

--- src/languages.h
+++ src/languages.h
@@ -9,9 +9,10 @@
 #define LANGUAGE_TR 6
 #define LANGUAGE_CZ 7
 #define LANGUAGE_PT 8
+#define LANGUAGE_GK 9
 
 /* day names in many different languages! */
-char dayNames[9][7][8] = {
+char dayNames[10][7][8] = {
 	{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"},
 	{"DIM", "LUN", "MAR", "MER", "JEU", "VEN", "SAM"},
 	{"SO", "MO", "DI", "MI", "DO", "FR", "SA"},
@@ -20,11 +21,12 @@ char dayNames[9][7][8] = {
 	{"ZO", "MA", "DI", "WO", "DO", "VR", "ZA"},
 	{"PAZ", "PTS", "SAL", "ÇAR", "PER", "CUM", "CTS"},
 	{"NE", "PO", "ÚT", "ST", "ČT", "PÁ", "SO"},
-	{"DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"}
+	{"DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"},
+	{"ΚΥΡ", "ΔΕΥ", "ΤΡΙ", "ΤΕΤ", "ΠΕΜ", "ΠΑΡ", "ΣΑΒ"}
 };
 
 /* month names in many different languages! */
-char monthNames[9][12][8] = {
+char monthNames[10][12][8] = {
 	{"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"},
 	{"JAN", "FÉV", "MAR", "AVR", "MAI", "JUI", "JUL", "AOÛ", "SEP", "OCT", "NOV", "DÉC"},
 	{"JAN", "FEB", "MÄR", "APR", "MAI", "JUN", "JUL", "AUG", "SEP", "OKT", "NOV", "DEZ"},
@@ -33,5 +35,6 @@ char monthNames[9][12][8] = {
 	{"JAN", "FEB", "MRT", "APR", "MEI", "JUN", "JUL", "AUG", "SEP", "OKT", "NOV", "DEC"},
 	{"OCA", "ŞUB", "MAR", "NİS", "MAY", "HAZ", "TEM", "AĞU", "EYL", "EKİ", "KAS", "ARA"},
 	{"LED", "ÚNO", "BŘE", "DUB", "KVĚ", "ČRV", "ČVC", "SRP", "ZÁŘ", "ŘÍJ", "LIS", "PRO"},
-	{"JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"}
+	{"JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"},
+	{"ΙΑΝ", "ΦΕΒ", "ΜΑΡ", "ΑΠΡ", "ΜΑΪ", "ΙΟΝ", "ΙΟΛ", "ΑΥΓ", "ΣΕΠ", "ΟΚΤ", "ΝΟΕ", "ΔΕΚ"}
 };
