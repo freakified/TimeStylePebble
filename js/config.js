3afbc1a2d176c4b411d0a3aaf12391803b48ad23
Dan Tilden <dtilden@sicom.com>
2015-08-13 06:41:09 -0400

    Improvements to sharing, experimental corrected color view

--- js/config.js
+++ js/config.js
@@ -9,16 +9,18 @@ function loadLastUsedColors() {
   // try to load each of the four colors
   var colorNames = ['time-color', 'time-bg-color', 'sidebar-color', 'sidebar-text-color'];
 
-  colorNames.forEach(function(colorName, index, array) {
+  for(i = 0; i < colorNames.length; i++) {
+    colorName = colorNames[i];
+
     var storedColor = window.localStorage.getItem(colorName);
 
     if(storedColor) {
       $('#' + colorName).spectrum("set", storedColor);
       $('#' + colorName).data('storedColor', storedColor);
-    } else {
+    } else  {
       $('#' + colorName).spectrum("set", null);
     }
-  });
+  }
 
   // in case anything changed
   updateCustomPreview();
@@ -300,7 +302,7 @@ var template = '\
 </label>\
 <div class="theme_actions">\
 <button class="btn btn-danger btn-sm" onclick="deletePreset({{preset-id}})" type="button"><span class="glyphicon glyphicon-trash"></span> Delete</button>\
-<button class="btn btn-primary btn-sm" type="button" data-toggle="modal" data-target="#shareThemeModal" data-themestring="TimeStyleTheme,{{time-color}},{{time-bg-color}},{{sidebar-color}},{{sidebar-text-color}}">\
+<button class="btn btn-info btn-sm" type="button" data-toggle="modal" data-target="#shareThemeModal" data-themestring="TimeStyleTheme,{{time-color}},{{time-bg-color}},{{sidebar-color}},{{sidebar-text-color}}">\
   <span class="glyphicon glyphicon-share-alt"></span> Share</span>\
 </button>\
 </div>\
@@ -384,8 +386,112 @@ function loadPastedTheme() {
 
 // support the individual "share" entry point from the preset selector
 $('#shareThemeModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var themeString = button.data('themestring')
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var themeString = button.data('themestring');
+
+  var mailToParams = {};
+
+  mailToParams.address = "";
+  mailToParams.subject = "TimeStyle Theme";
+  mailToParams.body    = themeString;
+
+  $('#share_via_email').attr('href', generateMailString(mailToParams));
+
+  mailToParams.address = "freakified+themesuggestion@gmail.com";
+  mailToParams.subject = "TimeStyle Theme Suggestion";
+
+  $('#suggest_as_preset').attr('href', generateMailString(mailToParams));
+
 
   $('#sharable_theme').html(themeString);
-})
+
+});
+
+function generateMailString(mailToParams) {
+  var mailToURL = "mailto:";
+
+  mailToURL += encodeURIComponent(mailToParams.address);
+  mailToURL += "?subject=";
+  mailToURL += encodeURIComponent(mailToParams.subject);
+  mailToURL += "&body=";
+  mailToURL += encodeURIComponent(mailToParams.body);
+
+  return mailToURL;
+}
+
+var colorMappingSunlight = {
+  "#000000": "#000000",
+  "#000055": "#001e41",
+  "#0000aa": "#004387",
+  "#0000ff": "#0068ca",
+  "#005500": "#2b4a2c",
+  "#005555": "#27514f",
+  "#0055aa": "#16638d",
+  "#0055ff": "#007dce",
+  "#00aa00": "#5e9860",
+  "#00aa55": "#5c9b72",
+  "#00aaaa": "#57a5a2",
+  "#00aaff": "#4cb4db",
+  "#00ff00": "#8ee391",
+  "#00ff55": "#8ee69e",
+  "#00ffaa": "#8aebc0",
+  "#00ffff": "#84f5f1",
+  "#550000": "#4a161b",
+  "#550055": "#482748",
+  "#5500aa": "#40488a",
+  "#5500ff": "#2f6bcc",
+  "#555500": "#564e36",
+  "#555555": "#545454",
+  "#5555aa": "#4f6790",
+  "#5555ff": "#4180d0",
+  "#55aa00": "#759a64",
+  "#55aa55": "#759d76",
+  "#55aaaa": "#71a6a4",
+  "#55aaff": "#69b5dd",
+  "#55ff00": "#9ee594",
+  "#55ff55": "#9de7a0",
+  "#55ffaa": "#9becc2",
+  "#55ffff": "#95f6f2",
+  "#aa0000": "#99353f",
+  "#aa0055": "#983e5a",
+  "#aa00aa": "#955694",
+  "#aa00ff": "#8f74d2",
+  "#aa5500": "#9d5b4d",
+  "#aa5555": "#9d6064",
+  "#aa55aa": "#9a7099",
+  "#aa55ff": "#9587d5",
+  "#aaaa00": "#afa072",
+  "#aaaa55": "#aea382",
+  "#aaaaaa": "#ababab",
+  "#ffffff": "#ffffff",
+  "#aaaaff": "#a7bae2",
+  "#aaff00": "#c9e89d",
+  "#aaff55": "#c9eaa7",
+  "#aaffaa": "#c7f0c8",
+  "#aaffff": "#c3f9f7",
+  "#ff0000": "#e35462",
+  "#ff0055": "#e25874",
+  "#ff00aa": "#e16aa3",
+  "#ff00ff": "#de83dc",
+  "#ff5500": "#e66e6b",
+  "#ff5555": "#e6727c",
+  "#ff55aa": "#e37fa7",
+  "#ff55ff": "#e194df",
+  "#ffaa00": "#f1aa86",
+  "#ffaa55": "#f1ad93",
+  "#ffaaaa": "#efb5b8",
+  "#ffaaff": "#ecc3eb",
+  "#ffff00": "#ffeeab",
+  "#ffff55": "#fff1b5",
+  "#ffffaa": "#fff6d3"
+};
+
+function showCorrectedColors() {
+  $('#preset_selector label').each(function() {
+    var me = $(this);
+
+    $('.example_face', me).css('color', colorMappingSunlight[me.data('time-color').toLowerCase()]);
+    $('.example_face', me).css('background-color', colorMappingSunlight[me.data('time-bg-color').toLowerCase()]);
+    $('.example_face', me).css('border-color', colorMappingSunlight[me.data('sidebar-color').toLowerCase()]);
+  });
+}
