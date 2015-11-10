b11c48e7cdc81ce42065a8db65ba1ddfa780cae0
Ramūnas Bunokas <brunonas@gmail.com>
2015-11-10 15:13:59 +0200

    Update languages.h

--- src/languages.h
+++ src/languages.h
@@ -22,13 +22,14 @@
 #define LANGUAGE_EU 19
 #define LANGUAGE_FI 20
 #define LANGUAGE_DA 21
+#define LANGUAGE_LT 22
 
 /* day names in many different languages! */
-extern const char dayNames[22][7][8];
+extern const char dayNames[23][7][8];
 
 /* month names in many different languages! */
-extern const char monthNames[22][12][8];
+extern const char monthNames[23][12][8];
 
 // all of these are taken from:
 // http://www.unicode.org/cldr/charts/28/by_type/date_&_time.fields.html#521165cf49647551
-extern const char wordForWeek[22][12];
+extern const char wordForWeek[23][12];
