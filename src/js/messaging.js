c450d608506110a6ff8bdce9d34b40ed83db70f8
Dan Tilden <tilden@outlook.com>
2015-11-16 07:54:07 -0500

    Restored config URL

--- src/js/messaging.js
+++ src/js/messaging.js
@@ -1,7 +1,7 @@
 var APP_VERSION = 4;
 // var BASE_CONFIG_URL = 'http://localhost:4000/';
 // var BASE_CONFIG_URL = 'http://192.168.1.105:4000/';
-var BASE_CONFIG_URL = 'http://brunas.github.io/TimeStylePebble/';
+var BASE_CONFIG_URL = 'http://freakified.github.io/TimeStylePebble/';
 
 var failureRetryAmount = 3;
 var currentFailures = 0;
