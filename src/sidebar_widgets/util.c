e07891bae8d6d7fa133255d02c74a0941ba7f7cc
Dan Tilden <tilden@outlook.com>
2016-02-21 14:58:19 -0500

    Switched sleep detection back to simple version

--- src/sidebar_widgets/util.c
+++ src/sidebar_widgets/util.c
@@ -19,22 +19,3 @@ void gdraw_command_image_recolor(GDrawCommandImage *img, GColor fill_color, GCol
   gdraw_command_list_iterate(gdraw_command_image_get_command_list(img),
                              recolor_iterator_cb, &colors);
 }
-
-#ifdef PBL_HEALTH
-  bool activity_search_cb(HealthActivity activity, time_t time_start, time_t time_end, void *context) {
-    bool *result = (bool *)context;
-    *result = true;
-
-    // if this gets called, we know that the activity we're searching for exists, so we call off the search
-    return false;
-   }
-
-  bool health_service_is_activity_in_range(HealthActivityMask activity_mask, time_t time_start, time_t time_end) {
-    bool result = false;
-
-     health_service_activities_iterate(activity_mask, time_start, time_end,
-      HealthIterationDirectionPast, activity_search_cb, &result);
-
-    return result;
-   }
- #endif
