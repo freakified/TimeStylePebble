dfe00152451d1ed8d9f590860c91dd9ba04bff03
Dan Tilden <tilden@outlook.com>
2017-01-11 09:31:51 -0500

    Fixed debug line

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -2,8 +2,8 @@
 var weather = require('./weather');
 
 var CONFIG_VERSION = 8;
-var BASE_CONFIG_URL = 'http://localhost:4000/';
-// var BASE_CONFIG_URL = 'http://freakified.github.io/TimeStylePebble/';
+// var BASE_CONFIG_URL = 'http://localhost:4000/';
+var BASE_CONFIG_URL = 'http://freakified.github.io/TimeStylePebble/';
 
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready',
