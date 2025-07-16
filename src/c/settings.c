20a0167d88c20a6c8b58e75ce566e8291b66be2f
Dan Tilden <tilden@outlook.com>
2025-07-16 09:15:11 -0400

    Remove old settings migration code

--- src/c/settings.c
+++ src/c/settings.c
@@ -4,160 +4,47 @@
 Settings globalSettings;
 
 void Settings_init() {
-  // first, check if we have any saved settings
-  // int settingsVersion = persist_read_int(SETTINGS_VERSION_KEY);
-
-  // load all settings
   Settings_loadFromStorage();
 }
 
 void Settings_deinit() {
-  // write all settings to storage
   Settings_saveToStorage();
 }
 
-/*
- * Load the saved color settings, or if they don't exist load defaults
- */
 void Settings_loadFromStorage() {
-  // load the default colors
-  globalSettings.timeBgColor      = GColorBlack;
+  globalSettings.timeBgColor = GColorBlack;
   globalSettings.sidebarTextColor = GColorBlack;
 
   #ifdef PBL_COLOR
-    globalSettings.timeColor      = GColorOrange;
-    globalSettings.sidebarColor   = GColorOrange;
+    globalSettings.timeColor = GColorOrange;
+    globalSettings.sidebarColor = GColorOrange;
   #else
-    globalSettings.timeColor      = GColorWhite;
-    globalSettings.sidebarColor   = GColorLightGray;
+    globalSettings.timeColor = GColorWhite;
+    globalSettings.sidebarColor = GColorLightGray;
   #endif
 
-  // set the default widgets
   globalSettings.widgets[0] = PBL_IF_HEALTH_ELSE(HEALTH, BATTERY_METER);
   globalSettings.widgets[1] = EMPTY;
   globalSettings.widgets[2] = DATE;
 
-  // set default disconnect icon activation
   globalSettings.activateDisconnectIcon = true;
-
   strncpy(globalSettings.altclockName, "ALT", sizeof(globalSettings.altclockName));
   globalSettings.decimalSeparator = '.';
-
   globalSettings.showBatteryPct = true;
 
-  int current_settings_version = persist_exists(SETTINGS_VERSION_KEY) ? persist_read_int(SETTINGS_VERSION_KEY) : -1;
-  APP_LOG(APP_LOG_LEVEL_DEBUG,"current_settings_version: %d", current_settings_version);
-  if( current_settings_version > 5 ) {
-    // new settings format
+  if (persist_exists(SETTING_VERSION6_AND_HIGHER)) {
     StoredSettings storedSettings;
-    memset(&storedSettings,0,sizeof(StoredSettings));
-    // if previous version settings are used than only first part of settings would be overwritten,
-    // all the other fields will left filled with zeroes
     persist_read_data(SETTING_VERSION6_AND_HIGHER, &storedSettings, sizeof(StoredSettings));
-    globalSettings.timeColor = storedSettings.timeColor;
-    globalSettings.timeBgColor = storedSettings.timeBgColor;
-    globalSettings.sidebarColor = storedSettings.sidebarColor;
-    globalSettings.sidebarTextColor = storedSettings.sidebarTextColor;
-    globalSettings.languageId = storedSettings.languageId;
-    globalSettings.showLeadingZero = storedSettings.showLeadingZero;
-    globalSettings.clockFontId = storedSettings.clockFontId;
-    globalSettings.btVibe = storedSettings.btVibe;
-    globalSettings.hourlyVibe = storedSettings.hourlyVibe;
-    globalSettings.widgets[0] = storedSettings.widgets[0];
-    globalSettings.widgets[1] = storedSettings.widgets[1];
-    globalSettings.widgets[2] = storedSettings.widgets[2];
-    globalSettings.sidebarOnLeft = storedSettings.sidebarOnLeft;
-    globalSettings.useLargeFonts = storedSettings.useLargeFonts;
-    globalSettings.useMetric = storedSettings.useMetric;
-    globalSettings.showBatteryPct = storedSettings.showBatteryPct;
-    globalSettings.disableAutobattery = storedSettings.disableAutobattery;
-    globalSettings.healthUseDistance = storedSettings.healthUseDistance;
-    globalSettings.healthUseRestfulSleep = storedSettings.healthUseRestfulSleep;
-    globalSettings.decimalSeparator = storedSettings.decimalSeparator;
-    memcpy(globalSettings.altclockName, storedSettings.altclockName, 8);
-    globalSettings.altclockOffset = storedSettings.altclockOffset;
-    globalSettings.activateDisconnectIcon = storedSettings.activateDisconnectIcon;
-  } else if( current_settings_version >= 0 ) {
-    // old settings format
-    if(persist_exists(SETTING_TIME_COLOR_KEY) && persist_exists(SETTING_TIME_BG_COLOR_KEY) &&
-       persist_exists(SETTING_SIDEBAR_COLOR_KEY) && persist_exists(SETTING_SIDEBAR_TEXT_COLOR_KEY)) {
-
-      // if the color data exists, load the colors
-      persist_read_data(SETTING_TIME_COLOR_KEY,         &globalSettings.timeColor,        sizeof(GColor));
-      persist_read_data(SETTING_TIME_BG_COLOR_KEY,      &globalSettings.timeBgColor,      sizeof(GColor));
-      persist_read_data(SETTING_SIDEBAR_COLOR_KEY,      &globalSettings.sidebarColor,     sizeof(GColor));
-      persist_read_data(SETTING_SIDEBAR_TEXT_COLOR_KEY, &globalSettings.sidebarTextColor, sizeof(GColor));
-    }
-
-    // load widgets
-    if(persist_exists(SETTING_SIDEBAR_WIDGET0_KEY)) {
-      globalSettings.widgets[0] = persist_read_int(SETTING_SIDEBAR_WIDGET0_KEY);
-      globalSettings.widgets[1] = persist_read_int(SETTING_SIDEBAR_WIDGET1_KEY);
-      globalSettings.widgets[2] = persist_read_int(SETTING_SIDEBAR_WIDGET2_KEY);
-    }
-
-    if(persist_exists(SETTING_ALTCLOCK_NAME_KEY)) {
-      persist_read_string(SETTING_ALTCLOCK_NAME_KEY, globalSettings.altclockName, sizeof(globalSettings.altclockName));
-    }
-
-    // load the rest of the settings, using default settings if none exist
-    // all settings except colors automatically return "0" or "false" if
-    // they haven't been set yet, so we don't need to check if they exist
-    globalSettings.useMetric              = persist_read_bool(SETTING_USE_METRIC_KEY);
-    globalSettings.sidebarOnLeft          = persist_read_bool(SETTING_SIDEBAR_LEFT_KEY);
-    globalSettings.btVibe                 = persist_read_bool(SETTING_BT_VIBE_KEY);
-    globalSettings.languageId             = persist_read_int(SETTING_LANGUAGE_ID_KEY);
-    globalSettings.showLeadingZero        = persist_read_int(SETTING_LEADING_ZERO_KEY);
-    globalSettings.showBatteryPct         = persist_read_bool(SETTING_SHOW_BATTERY_PCT_KEY);
-    globalSettings.disableAutobattery     = persist_read_bool(SETTING_DISABLE_AUTOBATTERY);
-    globalSettings.disableWeather         = persist_read_bool(SETTING_DISABLE_WEATHER_KEY);
-    globalSettings.clockFontId            = persist_read_int(SETTING_CLOCK_FONT_ID_KEY);
-    globalSettings.hourlyVibe             = persist_read_int(SETTING_HOURLY_VIBE_KEY);
-    globalSettings.useLargeFonts          = persist_read_bool(SETTING_USE_LARGE_FONTS_KEY);
-    globalSettings.altclockOffset         = persist_read_int(SETTING_ALTCLOCK_OFFSET_KEY);
-    globalSettings.healthUseDistance      = persist_read_bool(SETTING_HEALTH_USE_DISTANCE);
-    globalSettings.healthUseRestfulSleep  = persist_read_bool(SETTING_HEALTH_USE_RESTFUL_SLEEP);
-
-    if(persist_exists(SETTING_DECIMAL_SEPARATOR_KEY)) {
-      globalSettings.decimalSeparator = (char)persist_read_int(SETTING_DECIMAL_SEPARATOR_KEY);
-    }
+    memcpy(&globalSettings, &storedSettings, sizeof(StoredSettings));
   }
 
   Settings_updateDynamicSettings();
 }
 
 void Settings_saveToStorage() {
-  // ensure that the weather disabled setting is accurate before saving it
   Settings_updateDynamicSettings();
-
-  // save settings to compressed structure and to persistent storage
   StoredSettings storedSettings;
-  // if previous version settings are used than only first part of settings would be overwrited
-  // all the other fields will left filled with zeroes
-  storedSettings.timeColor = globalSettings.timeColor;
-  storedSettings.timeBgColor = globalSettings.timeBgColor;
-  storedSettings.sidebarColor = globalSettings.sidebarColor;
-  storedSettings.sidebarTextColor = globalSettings.sidebarTextColor;
-  storedSettings.languageId = globalSettings.languageId;
-  storedSettings.showLeadingZero = globalSettings.showLeadingZero;
-  storedSettings.clockFontId = globalSettings.clockFontId;
-  storedSettings.btVibe = globalSettings.btVibe;
-  storedSettings.hourlyVibe = globalSettings.hourlyVibe;
-  storedSettings.widgets[0] = globalSettings.widgets[0];
-  storedSettings.widgets[1] = globalSettings.widgets[1];
-  storedSettings.widgets[2] = globalSettings.widgets[2];
-  storedSettings.sidebarOnLeft = globalSettings.sidebarOnLeft;
-  storedSettings.useLargeFonts = globalSettings.useLargeFonts;
-  storedSettings.useMetric = globalSettings.useMetric;
-  storedSettings.showBatteryPct = globalSettings.showBatteryPct;
-  storedSettings.disableAutobattery = globalSettings.disableAutobattery;
-  storedSettings.healthUseDistance = globalSettings.healthUseDistance;
-  storedSettings.healthUseRestfulSleep = globalSettings.healthUseRestfulSleep;
-  storedSettings.decimalSeparator = globalSettings.decimalSeparator;
-  memcpy(storedSettings.altclockName, globalSettings.altclockName, 8);
-  storedSettings.altclockOffset = globalSettings.altclockOffset;
-  storedSettings.activateDisconnectIcon = globalSettings.activateDisconnectIcon;
-
+  memcpy(&storedSettings, &globalSettings, sizeof(StoredSettings));
   persist_write_data(SETTING_VERSION6_AND_HIGHER, &storedSettings, sizeof(StoredSettings));
   persist_write_int(SETTINGS_VERSION_KEY, CURRENT_SETTINGS_VERSION);
 }
--- src/c/settings.h
+++ src/c/settings.h
@@ -3,13 +3,11 @@
 #include "sidebar_widgets.h"
 
 #define SETTINGS_VERSION_KEY 4
-
-// settings "version" for app version 4.0
 #define CURRENT_SETTINGS_VERSION 6
 
 typedef enum {
-  NO_VIBE              = 0,
-  VIBE_EVERY_HOUR      = 1,
+  NO_VIBE = 0,
+  VIBE_EVERY_HOUR = 1,
   VIBE_EVERY_HALF_HOUR = 2
 } VibeIntervalType;
 
@@ -58,7 +56,6 @@ typedef struct {
   bool enableBeats;
   bool enableAltTimeZone;
 
-  // TODO: these shouldn't be dynamic
   GColor iconFillColor;
   GColor iconStrokeColor;
 } Settings;
@@ -107,51 +104,7 @@ typedef struct {
 } StoredSettings;
 
 extern Settings globalSettings;
-
-// persistent storage keys for each setting
-
-// color settings
-#define SETTING_TIME_COLOR_KEY            17
-#define SETTING_TIME_BG_COLOR_KEY         18
-#define SETTING_SIDEBAR_COLOR_KEY         19
-#define SETTING_SIDEBAR_TEXT_COLOR_KEY    20
-
-// general settings
-#define SETTING_LANGUAGE_ID_KEY           25
-#define SETTING_LEADING_ZERO_KEY          10
-#define SETTING_CLOCK_FONT_ID_KEY         13
-
-// vibration settings
-#define SETTING_BT_VIBE_KEY               23
-#define SETTING_HOURLY_VIBE_KEY           14
-
-// sidebar settings
-#define SETTING_SIDEBAR_WIDGET0_KEY       26
-#define SETTING_SIDEBAR_WIDGET1_KEY       27
-#define SETTING_SIDEBAR_WIDGET2_KEY       28
-#define SETTING_SIDEBAR_LEFT_KEY          22
-#define SETTING_USE_LARGE_FONTS_KEY       16
-
-// weather widget settings
-#define SETTING_DISABLE_WEATHER_KEY       12
-#define SETTING_USE_METRIC_KEY            21
-
-// battery meter widget settings
-#define SETTING_SHOW_BATTERY_PCT_KEY      11
-#define SETTING_DISABLE_AUTOBATTERY       36
-
-// alt tz widget settings
-#define SETTING_ALTCLOCK_NAME_KEY         30
-#define SETTING_ALTCLOCK_OFFSET_KEY       31
-
-// health widget settings
-#define SETTING_HEALTH_USE_DISTANCE       32
-#define SETTING_HEALTH_USE_RESTFUL_SLEEP  33
-#define SETTING_HEALTH_USE_METRIC         35
-#define SETTING_DECIMAL_SEPARATOR_KEY     34
-
-// key for all the settings for versions 6 and higher
-#define SETTING_VERSION6_AND_HIGHER       100
+#define SETTING_VERSION6_AND_HIGHER 100
 
 void Settings_init();
 void Settings_deinit();
