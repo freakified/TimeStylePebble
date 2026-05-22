e634f465bdd6250aa6826eeb13014c7a698a5e55
CaCO3 <caco@ruinelli.ch>
2026-05-22 22:59:12 +0200

    Fix build error: remove enum, add keys to package.json
    - Remove enum from messaging.h (keys auto-generated from package.json)
    - Add BatteryPercent and IsCharging to messageKeys in package.json
    - This fixes redeclaration errors during build

--- src/c/messaging.h
+++ src/c/messaging.h
@@ -1,41 +1,6 @@
 #pragma once
 #include <pebble.h>
 
-// Message keys
-enum {
-  MESSAGE_KEY_WeatherTemperature = 0,
-  MESSAGE_KEY_WeatherCondition,
-  MESSAGE_KEY_WeatherUVIndex,
-  MESSAGE_KEY_WeatherForecastCondition,
-  MESSAGE_KEY_WeatherForecastHighTemp,
-  MESSAGE_KEY_WeatherForecastLowTemp,
-  MESSAGE_KEY_SettingColorTime,
-  MESSAGE_KEY_SettingColorBG,
-  MESSAGE_KEY_SettingColorSidebar,
-  MESSAGE_KEY_SettingSidebarOnLeft,
-  MESSAGE_KEY_SettingSidebarTextColor,
-  MESSAGE_KEY_SettingUseMetric,
-  MESSAGE_KEY_SettingBluetoothVibe,
-  MESSAGE_KEY_SettingLanguageID,
-  MESSAGE_KEY_SettingShowLeadingZero,
-  MESSAGE_KEY_SettingShowBatteryPct,
-  MESSAGE_KEY_SettingClockFontId,
-  MESSAGE_KEY_SettingHourlyVibe,
-  MESSAGE_KEY_SettingUseLargeFonts,
-  MESSAGE_KEY_SettingWidget0ID,
-  MESSAGE_KEY_SettingWidget1ID,
-  MESSAGE_KEY_SettingWidget2ID,
-  MESSAGE_KEY_SettingAltClockName,
-  MESSAGE_KEY_SettingAltClockOffset,
-  MESSAGE_KEY_SettingDecimalSep,
-  MESSAGE_KEY_SettingHealthUseDistance,
-  MESSAGE_KEY_SettingHealthUseRestfulSleep,
-  MESSAGE_KEY_SettingDisableAutobattery,
-  MESSAGE_KEY_SettingDisconnectIcon,
-  MESSAGE_KEY_BatteryPercent,
-  MESSAGE_KEY_IsCharging
-};
-
 void messaging_requestNewWeatherData();
 void messaging_sendBatteryData(uint8_t batteryPercent, bool isCharging);
 
